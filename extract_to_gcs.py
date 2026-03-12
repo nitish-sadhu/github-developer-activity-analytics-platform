b158cd6a436e6ebb73397119fc48e278f1a3d282
krishna <sadhunitish@gmail.com>
2026-03-12 20:06:04 +0530

    modified extract_to_gcs.py with script to upload to gcs

--- extract_to_gcs.py
+++ extract_to_gcs.py
@@ -1,30 +1,45 @@
 from google.cloud import storage
-
-import json 
 import requests
-import pandas as pd 
-from pathlib import Path 
-
 import logging 
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-logger = loggin.getLogger(__name__)
-
-BASE_BUCKET_PATH = "gcs://raw-github-dev-activity"
+BASE_BUCKET = "raw-github-dev-activity"
 BASE_URL = "https://data.gharchive.org"
 
 
-def download(date: str, hour: int) -> None:
+def extract_to_gcs(date: str, hour: int) -> None:
 
-	bucket_path = Path(BASE_GCS_PATH) / date / hour
+	storage_client = storage.Client()
+	bucket = storage_client.bucket(BASE_BUCKET)
+
 	url = f"{BASE_URL}/{date}-{hour}.json.gz"
+	blob_path = f"{date}/{date}-{hour}.json.gz"
+
 
 	try:
-		with requests.get(url, "wb") as f:
-			f.write(bucket_path)
+
+		logger.info(f"Starting Download - {date}-{hour}.json.gz")
+
+		response = requests.get(url)
+		response.raise_for_status()
+
+		logger.info(f"Starting upload to {BASE_BUCKET}")
+
+		blob = bucket.blob(blob_path)
+		blob.upload_from_string(response.content)
+
+	except requests.HTTPError as e:
+		logger.error(f"__ERROR__ : {e}")
+		raise
 
 	except Exception as e:
 		logger.error(f"___ERROR___ : {e}")
+		raise
 
 
 
+if __name__ == "__main__":
+# 2/12/2011
+	extract_to_gcs("2010-01-01", "0")
